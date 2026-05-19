ad7e111b1cd62927b0535bd611e34c880256ddc1
Pawan4356 <pawankumar27112005@gmail.com>
2026-05-19 20:31:01 +0530

    Resize and Crop

---
+++ 03_resizeCrop.py
@@ -0,0 +1,13 @@
+import cv2
+
+img = cv2.imread('media/image.png')
+print(img.shape)
+cv2.imshow('Image', img)
+
+img_resize = cv2.resize(img, (200, 500))
+cv2.imshow('Resized Image', img_resize)
+
+img_crop = img[:200, :200]
+cv2.imshow('Croped Image', img_crop)
+
+cv2.waitKey(0)
